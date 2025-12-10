736e44607db9fae9b54d9f2d0c0a563ea486070e
Victor Lopes <victorlopesteodoro@gmail.com>
2025-12-10 19:35:17 -0300

    feat: filtros

--- src/controllers/CategoriaController.js
+++ src/controllers/CategoriaController.js
@@ -4,11 +4,18 @@ const Produto = require('../models/Produto');
 module.exports = {
     async listar(req, res) {
         const id = req.query.id;
+        const ordem = req.query.ordem;
 
         const categoria = await Categoria.buscarPorId(id);
         if (!categoria) return res.send("Categoria não encontrada");
 
-        const produtos = await Produto.produtosPorCategoria(id);
+        let produtos = await Produto.produtosPorCategoria(id);
+
+        if (ordem === 'asc') {
+            produtos.sort((a, b) => a.preco - b.preco);
+        } else if (ordem === 'desc') {
+            produtos.sort((a, b) => b.preco - a.preco);
+        }
 
         res.render('categoria', {
             categoria,
--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -3,8 +3,16 @@ const Categoria = require('../models/Categoria');
 
 module.exports = {
     async index(req, res) {
-        const produtos = await Produto.buscarAtivos();
+        const ordem = req.query.ordem;
+
+        let produtos = await Produto.buscarAtivos();
         const categorias = await Categoria.todas();
+
+        if (ordem === 'asc') {
+            produtos.sort((a, b) => a.preco - b.preco);
+        } else if (ordem === 'desc') {
+            produtos.sort((a, b) => b.preco - a.preco);
+        }
 
         res.render('home', {
             produtos,
